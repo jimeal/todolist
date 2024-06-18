7a7df18f2c5e34e0764647a5a288c4835e0aef7b
Bae Ji-min <dncl46@gmail.com>
2024-06-19 01:58:48 +0900

    useMutation을 이용한 TodoList 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import './App.css'
-import { useQuery } from '@apollo/client'
-import { GET_TODOS } from './apollo/todos'
+import { useMutation, useQuery } from '@apollo/client'
+import { ADD_TODO, GET_TODOS } from './apollo/todos'
 import { useState } from 'react';
 import TodoItem from './components/TodoItem';
-import { IList } from './types';
+import { AllTodosCache, IList } from './types';
 
 function App() {
   const { loading, error, data } = useQuery(GET_TODOS);
-  console.log(loading, error, data);
   const [input, setInput] = useState("");
+
+  const [addTodo, { error: addError }] = useMutation(ADD_TODO, {
+    update(cache, { data: { createTodo } }) {
+      const previousTodos = cache.readQuery<AllTodosCache>({ query: GET_TODOS })?.allTodos;
+      cache.writeQuery({
+        query: GET_TODOS,
+        data: {
+          allTodos: [createTodo, ...previousTodos as IList[]],
+        },
+      });
+    }
+  })
+
   const counter = ():string => {
     if(data?.allTodos as IList[]){
       const completed = (data.allTodos).filter((todo: IList) => todo.checked);
@@ -17,13 +29,29 @@ function App() {
     return "0/0"
   }
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(input.trim() === "") return;
+    addTodo({
+      variables: {
+        text: input,
+        checked: false
+      }
+    })
+
+    setInput('')
+  }
+
   if (error) return <div>NETWORK ERROR</div>
   return (
     <>
       <div className="flex flex-col items-center">
         <div className="mt-5 text-3xl">Todo App{" "}<span className='text-sm'>({counter()})</span></div>
         <div className="w-5/6 md:w-1/2 lg:w-3/5">
-          <form className='flex justify-between p-5 my-5 text-4xl border-2 rounded-md shadow-md'>
+          <form 
+              className='flex justify-between p-5 my-5 text-4xl border-2 rounded-md shadow-md'
+              onSubmit={handleSubmit}
+          >
             <input placeholder='할일을 작성해 주세요' type={"text"} className='outline-none border-b-[1px] text-xl w-10/12 focus:border-b-[3px]' value={input} onChange={(e) => setInput(e.target.value)} />
             <div>
               <button type='submit' className='hover:scale-105'>+</button>
