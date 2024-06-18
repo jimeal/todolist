7754a119642a4657716bd9244be2552fe8f7b2c8
Bae Ji-min <dncl46@gmail.com>
2024-06-19 00:49:32 +0900

    useQuery룰 이용한 todo 데이터 가져오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import './App.css'
+import { useQuery } from '@apollo/client'
+import { GET_TODOS } from './apollo/todos'
+import { useState } from 'react';
+import TodoItem from './components/TodoItem';
+import { IList } from './types';
 
 function App() {
+  const { loading, error, data } = useQuery(GET_TODOS);
+  console.log(loading, error, data);
+  const [input, setInput] = useState("");
+  const counter = ():string => {
+    if(data?.allTodos as IList[]){
+      const completed = (data.allTodos).filter((todo: IList) => todo.checked);
+      return `${completed.length}/${(data.allTodos).length}`;
+    }
+    return "0/0"
+  }
 
+  if (error) return <div>NETWORK ERROR</div>
   return (
     <>
-      <div className="App">
-        <p className="underline">안녕하세요</p>
+      <div className="flex flex-col items-center">
+        <div className="mt-5 text-3xl">Todo App{" "}<span className='text-sm'>({counter()})</span></div>
+        <div className="w-5/6 md:w-1/2 lg:w-3/5">
+          <form className='flex justify-between p-5 my-5 text-4xl border-2 rounded-md shadow-md'>
+            <input placeholder='할일을 작성해 주세요' type={"text"} className='outline-none border-b-[1px] text-xl w-10/12 focus:border-b-[3px]' value={input} onChange={(e) => setInput(e.target.value)} />
+            <div>
+              <button type='submit' className='hover:scale-105'>+</button>
+            </div>
+          </form>
+
+          {loading ? <div>loading....</div> : 
+          <ul>
+            {data && 
+              data.allTodos.map((item:IList) => 
+                <TodoItem key={item.id} item={item} />
+              )
+            }  
+          </ul>}
+        </div>
       </div>
     </>
   )
--- src/apollo/todos.ts
+++ src/apollo/todos.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const GET_TODOS = gql`
-  guery getTodos {
+  query getTodos {
     allTodos {
       text
       checked
@@ -11,8 +11,8 @@ export const GET_TODOS = gql`
 `;
 
 export const ADD_TODO = gql`
-  mutation addTodo($text: String!, $checked: Boolean!) {
-    createTodo(text: $text!, checked: $checked) {
+  mutation addTodo($text:String!, $checked:Boolean!) {
+    createTodo(text:$text, checked:$checked) {
       text
       checked
       id
@@ -21,8 +21,8 @@ export const ADD_TODO = gql`
 `;
 
 export const UPDATE_TODO = gql`
-  mutation updateTodo($text: String, $checked: Boolean, $id: ID!) {
-    updateTodo(text: $text, checked: $checked, id: $id) {
+  mutation updateTodo($text:String, $checked:Boolean, $id:ID!) {
+    updateTodo(text:$text, checked:$checked, id:$id) {
       text
       checked
       id
@@ -31,9 +31,9 @@ export const UPDATE_TODO = gql`
 `;
 
 export const REMOVE_TODO = gql`
-  mutation removeTodo($id: ID) {
-    removeTodo(id: $id) {
+  mutation removeTodo($id:ID!) {
+    removeTodo(id:$id) {
       id
     }
   }
-`
+`;
